96e2b06c836b88a78c414d49aefe2c9974096239
ajanujmax <ajanujmax@gmail.com>
2021-03-21 00:36:56 +0530

    string o/p and basic arithmatic ops

--- hello.py
+++ hello.py
@@ -6,3 +6,19 @@ print("hello world")
 this is 
 multi line comment
 '''
+
+#string line
+str= "this is me"
+#interger
+age = 21
+# float
+ weight = 75.6
+
+# Number Stiring List Tuples Dictionary
+#arithmatic opreation
+print ("the value of 3 + 5 = ", 3+5) #add
+print ("the value of 3 - 5 = ", 3-5) # sub
+print ("the value of 3 * 5 = ", 3*5) #multiple
+print ("the value of 3 / 5 = ", 3/5) #divide
+print ("the value of 3 ** 5 = ", 3**5) #power
+print ("the value of 3 // 5 = ", 3//5) #float division
