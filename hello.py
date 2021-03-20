fb57e278d16b375ed2508229b2984f009fdf382a
ajanujmax <ajanujmax@gmail.com>
2021-03-21 00:41:51 +0530

    multi line string

--- hello.py
+++ hello.py
@@ -22,3 +22,12 @@ print ("the value of 3 * 5 = ", 3*5) #multiple
 print ("the value of 3 / 5 = ", 3/5) #divide
 print ("the value of 3 ** 5 = ", 3**5) #power
 print ("the value of 3 // 5 = ", 3//5) #float division
+
+print("this is a \"")  #print of double quote
+
+msl = ''' 
+this is  
+multi line string and 
+its keep going 
+'''
+print(msl) #multi line string 
