1c8e2bb6e73d2e71f8b983b4c885177d00a35980
ajanujmax <ajanujmax@gmail.com>
2021-03-21 01:02:51 +0530

    list:- declare, print,update

---
+++ list.py
@@ -0,0 +1,20 @@
+#LISTS --------------------------------------------------
+# example
+college = ['IIT ', ' NIT', 'COLLEGE OF ENGINNERING']
+print(college[2])
+
+#   output
+#   COLLEGE OF ENGINNERING
+
+#--------------------------------------------------
+#To upadate list
+college[2]= "COE"
+
+#   output
+#    COE
+
+#------------------------------------------------------
+#print style
+print(college[2]) # print only 2 i.e COE
+print(college) # all printed 
+print(college[1:3]) # this will skip 1st postion(0 index)
