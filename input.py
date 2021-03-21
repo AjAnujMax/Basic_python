e28e25e5ed29479c140f5abc8283cbb9b7529921
ajanujmax <ajanujmax@gmail.com>
2021-03-21 22:42:41 +0530

    input statement and type cast

---
+++ input.py
@@ -0,0 +1,12 @@
+#-----------------------------------------
+#input
+print("enter ur number")
+number = input()
+print(number)
+
+#string to int type cast
+print("enter ur marks")
+number = int(input())
+print(number)
+
+
