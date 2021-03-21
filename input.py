4c2ec690429f2229185dfd84e339a0bc556f9e4e
ajanujmax <ajanujmax@gmail.com>
2021-03-21 22:52:56 +0530

    IF-ELSE WITH OR AND

--- input.py
+++ input.py
@@ -3,11 +3,15 @@
 print("enter ur number")
 number = input()
 print(number)
+#OUTPUT string
+# 10
 
 #string to int type cast
 print("enter ur marks")
 number = int(input())
 print(number)
+#OUTPUT int
+#10
 
 
 #IF-ELSE
@@ -21,3 +25,20 @@ elif (number>80):
 else:
         grade = 'Dont know'
 print("grade is",grade)
+#OUTPUT
+#grade is Dont know
+
+#-----------------------------------------------
+#IF-ELSE WITH OR AND 
+
+if (number>90 or number>80):
+    grade = 'A'
+    
+elif (number>80 and number<70):
+    grade = 'B'
+    
+else:
+        grade = 'Dont know'
+print("grade is",grade)
+#OUTPUT
+#grade is Dont know
