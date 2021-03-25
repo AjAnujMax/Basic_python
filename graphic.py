a6ea3dbed78a883f41bad8ec3aef46e72e32768b
Anuj <56772154+AjAnujMax@users.noreply.github.com>
2021-03-25 23:44:31 +0530

    pyplot intro

--- graphic.py
+++ graphic.py
@@ -8,4 +8,17 @@ print(matplotlib.__version__)
 
 #output:- 2.0.0
 
-#---------------------------------------------------------
+#---------------------Pyplot------------------------------------
+#import matplotlib.pyplot as plt
+#Now the Pyplot package can be referred to as plt.
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+
+
