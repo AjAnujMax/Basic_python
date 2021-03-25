9c94b11e738a8eb13f323832325a5eec663caecc
Anuj <56772154+AjAnujMax@users.noreply.github.com>
2021-03-25 23:53:12 +0530

    Format Strings fmt

--- graphic.py
+++ graphic.py
@@ -101,5 +101,14 @@ plt.show()
 
 
 
-#----------------------Matplotlib Markers ----------------------------------
+#----------------------Format Strings fmt ----------------------------------
+#shortcut string notation - keyword-fmt
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, 'o:r')
+plt.show()
+
 
