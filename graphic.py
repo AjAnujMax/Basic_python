65164e7e6353e3885febe099d98d718166825939
Anuj <56772154+AjAnujMax@users.noreply.github.com>
2021-03-25 23:47:40 +0530

    Pyplot-Default X-Points

--- graphic.py
+++ graphic.py
@@ -53,3 +53,12 @@ ypoints = np.array([3, 8, 1, 10])
 plt.plot(xpoints, ypoints)
 plt.show()
 
+#---------------------Pyplot-Default X-Points----------------------------------
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10, 5, 7])
+
+plt.plot(ypoints)
+plt.show()
+
