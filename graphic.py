52a4d03236dead09fb59949329bd3386599f97a9
Anuj <56772154+AjAnujMax@users.noreply.github.com>
2021-03-25 23:45:49 +0530

    Pyplot-Plotting Without Line

--- graphic.py
+++ graphic.py
@@ -21,4 +21,25 @@ plt.plot(xpoints, ypoints)
 plt.show()
 
 
+#---------------------Pyplot-Plotting x and y points----------------------------------
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+#---------------------Pyplot-Plotting Without Line----------------------------------
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
 
