b13915f0cef426f2e9f21736544e36038534a14d
Anuj <56772154+AjAnujMax@users.noreply.github.com>
2021-03-26 00:00:53 +0530

    Matplotlib Line

--- graphic.py
+++ graphic.py
@@ -132,3 +132,16 @@ ypoints = np.array([3, 8, 1, 10])
 plt.plot(ypoints, marker = 'o', ms = 20)
 plt.show()
 
+#---------------------Matplotlib Line ----------------------------------
+#keyword argument linestyle, or shorter ls
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, linestyle = 'dotted')
+plt.show()
+
+
+
