35bac690e0da5844cfc522adeae5fa1b7a548e4e
Anuj <56772154+AjAnujMax@users.noreply.github.com>
2021-03-25 23:46:44 +0530

    Pyplot-Plotting Multiple Points

--- graphic.py
+++ graphic.py
@@ -43,3 +43,13 @@ ypoints = np.array([3, 10])
 plt.plot(xpoints, ypoints, 'o')
 plt.show()
 
+#---------------------Pyplot-Plotting Multiple Points----------------------------------
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
