5bdf0e1a706a05f2389adc5a9a127cf3b0171219
Anuj <56772154+AjAnujMax@users.noreply.github.com>
2021-03-25 23:42:43 +0530

    initial

---
+++ graphic.py
@@ -0,0 +1,11 @@
+# What is Matplotlib?
+# Matplotlib is a low level graph plotting library in python that serves as a visualization utility.
+
+#--------------------intro------------------------------
+
+import matplotlib
+print(matplotlib.__version__)
+
+#output:- 2.0.0
+
+#---------------------------------------------------------
