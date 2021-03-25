a3ba43d4a339fd4a5b15c663bad69390479088d6
Anuj <56772154+AjAnujMax@users.noreply.github.com>
2021-03-25 23:52:08 +0530

    Matplotlib Markers

--- graphic.py
+++ graphic.py
@@ -62,3 +62,44 @@ ypoints = np.array([3, 8, 1, 10, 5, 7])
 plt.plot(ypoints)
 plt.show()
 
+#----------------------Matplotlib Markers ----------------------------------
+#keyword argument marker
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = 'o')
+plt.show()
+
+
+#plt.plot(ypoints, marker = '*')
+# 'o'	Circle	
+# '*'	Star	
+# '.'	Point	
+# ','	Pixel	
+# 'x'	X	
+# 'X'	X (filled)	
+# '+'	Plus	
+# 'P'	Plus (filled)	
+# 's'	Square	
+# 'D'	Diamond	
+# 'd'	Diamond (thin)	
+# 'p'	Pentagon	
+# 'H'	Hexagon	
+# 'h'	Hexagon	
+# 'v'	Triangle Down	
+# '^'	Triangle Up	
+# '<'	Triangle Left	
+# '>'	Triangle Right	
+# '1'	Tri Down	
+# '2'	Tri Up	
+# '3'	Tri Left	
+# '4'	Tri Right	
+# '|'	Vline	
+# '_'	Hline
+
+
+
+#----------------------Matplotlib Markers ----------------------------------
+
